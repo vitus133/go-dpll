2b46f94d9a14b5f924bb5e9c457038fd24c712a5
Vitaly Grinberg <v.g@redhat.com>
2024-02-22 11:32:36 +0000

    Add pin reading
    This commit adds DPLL pin read, dump and notifications
    
    Signed-off-by: Vitaly Grinberg <v.g@redhat.com>

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"strings"
 	"time"
 
+	"github.com/mdlayher/genetlink"
 	dpll "github.com/vitus133/go-dpll/pkg/dpll-ynl"
 )
 
@@ -14,11 +16,7 @@ func main() {
 		log.Fatal(err)
 	}
 	defer conn.Close()
-	mcastId, found := conn.GetMcastGroupId(dpll.DPLL_MCGRP_MONITOR)
-	if !found {
-		log.Panic("multicast ID ", dpll.DPLL_MCGRP_MONITOR, " not found")
-	}
-
+	log.Println("Dumping devices")
 	replies, err := conn.DumpDeviceGet()
 	if err != nil {
 		log.Panic(err)
@@ -26,6 +24,24 @@ func main() {
 	for _, reply := range replies {
 		log.Println(dpll.GetDpllStatusHR(reply))
 	}
+	log.Println("dumping pins")
+	pinReplies, err := conn.DumpPinGet()
+	if err != nil {
+		log.Panic(err)
+	}
+	for _, pinInfo := range pinReplies {
+		pinInfo, err := dpll.GetPinInfoHR(pinInfo)
+		if err != nil {
+			log.Panic(err)
+		}
+		fmt.Println(string(pinInfo))
+	}
+	log.Println("monitoring notifications")
+	mcastId, found := conn.GetMcastGroupId(dpll.DPLL_MCGRP_MONITOR)
+	if !found {
+		log.Panic("multicast ID ", dpll.DPLL_MCGRP_MONITOR, " not found")
+	}
+
 	c := conn.GetGenetlinkConn()
 	err = c.JoinGroup(mcastId)
 	if err != nil {
@@ -42,12 +58,35 @@ func main() {
 			log.Println(err)
 			continue
 		}
-		ntfs, err := dpll.ParseDeviceReplies(msgs)
-		if err != nil {
-			log.Panic(err)
-		}
-		for _, ntf := range ntfs {
-			log.Println(dpll.GetDpllStatusHR(ntf))
+		for _, msg := range msgs {
+			switch msg.Header.Command {
+			case dpll.DPLL_CMD_DEVICE_CHANGE_NTF:
+				ntfs, err := dpll.ParseDeviceReplies([]genetlink.Message{msg})
+				if err != nil {
+					log.Panic(err)
+				}
+				for _, ntf := range ntfs {
+					log.Println("")
+					fmt.Println(dpll.GetDpllStatusHR(ntf))
+				}
+			case dpll.DPLL_CMD_PIN_CHANGE_NTF:
+				ntfs, err := dpll.ParsePinReplies([]genetlink.Message{msg})
+				if err != nil {
+					log.Panic(err)
+				}
+				for _, ntf := range ntfs {
+					hr, err := dpll.GetPinInfoHR(ntf)
+					if err != nil {
+						log.Panic(err)
+					}
+					log.Println("")
+					fmt.Println(string(hr))
+				}
+			default:
+				log.Println("unsupported dpll message")
+
+			}
+
 		}
 	}
 }
--- pkg/dpll-ynl/dpll-uapi.go
+++ pkg/dpll-ynl/dpll-uapi.go
@@ -3,18 +3,38 @@
 
 package dpll
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 const DPLL_MCGRP_MONITOR = "monitor"
+const DPLL_PHASE_OFFSET_DIVIDER = 1000
+const DPLL_TEMP_DIVIDER = 1000
 const (
 	DPLL_A_TYPES = iota
 	DPLL_A_ID
 	DPLL_A_MODULE_NAME
+	DPLL_A_PAD
 	DPLL_A_CLOCK_ID
 	DPLL_A_MODE
 	DPLL_A_MODE_SUPPORTED
 	DPLL_A_LOCK_STATUS
 	DPLL_A_TEMP
 	DPLL_A_TYPE
+
+	__DPLL_A_MAX
+	DPLL_A_MAX = __DPLL_A_MAX - 1
+)
+
+const (
+	DPLL_A_PIN_TYPES = iota
+
 	DPLL_A_PIN_ID
+	DPLL_A_PIN_PARENT_ID
+	DPLL_A_PIN_MODULE_NAME
+	DPLL_A_PIN_PAD
+	DPLL_A_PIN_CLOCK_ID
 	DPLL_A_PIN_BOARD_LABEL
 	DPLL_A_PIN_PANEL_LABEL
 	DPLL_A_PIN_PACKAGE_LABEL
@@ -26,13 +46,18 @@ const (
 	DPLL_A_PIN_FREQUENCY_MAX
 	DPLL_A_PIN_PRIO
 	DPLL_A_PIN_STATE
-	DPLL_A_PIN_DPLL_CAPS
-	DPLL_A_PIN_PARENT
+	DPLL_A_PIN_CAPABILITIES
+	DPLL_A_PIN_PARENT_DEVICE
+	DPLL_A_PIN_PARENT_PIN
+	DPLL_A_PIN_PHASE_ADJUST_MIN
+	DPLL_A_PIN_PHASE_ADJUST_MAX
+	DPLL_A_PIN_PHASE_ADJUST
+	DPLL_A_PIN_PHASE_OFFSET
+	DPLL_A_PIN_FRACTIONAL_FREQUENCY_OFFSET
 
-	__DPLL_A_MAX
-	DPLL_A_MAX = __DPLL_A_MAX - 1
+	__DPLL_A_PIN_MAX
+	DPLL_A_PIN_MAX = __DPLL_A_PIN_MAX - 1
 )
-
 const (
 	DPLL_CMDS = iota
 	DPLL_CMD_DEVICE_ID_GET
@@ -53,8 +78,8 @@ const (
 )
 
 // GetLockStatus returns DPLL lock status as a string
-func GetLockStatus(ls uint8) string {
-	lockStatusMap := map[uint8]string{
+func GetLockStatus(ls uint32) string {
+	lockStatusMap := map[uint32]string{
 		1: "unlocked",
 		2: "locked",
 		3: "locked-ho-acquired",
@@ -68,7 +93,7 @@ func GetLockStatus(ls uint8) string {
 }
 
 // GetDpllType returns DPLL type as a string
-func GetDpllType(tp uint8) string {
+func GetDpllType(tp uint32) string {
 	typeMap := map[int]string{
 		1: "pps",
 		2: "eec",
@@ -81,7 +106,7 @@ func GetDpllType(tp uint8) string {
 }
 
 // GetMode returns DPLL mode as a string
-func GetMode(md uint8) string {
+func GetMode(md uint32) string {
 	modeMap := map[int]string{
 		1: "manual",
 		2: "automatic",
@@ -102,7 +127,7 @@ type DpllStatusHR struct {
 	Mode          string
 	ModeSupported string
 	LockStatus    string
-	ClockId       uint64
+	ClockId       string
 	Type          string
 }
 
@@ -112,9 +137,137 @@ func GetDpllStatusHR(reply *DoDeviceGetReply) DpllStatusHR {
 		Id:         reply.Id,
 		ModuleName: reply.ModuleName,
 		Mode:       GetMode(reply.Mode),
-		// TODO: ModeSupported
 		LockStatus: GetLockStatus(reply.LockStatus),
-		ClockId:    reply.ClockId,
+		ClockId:    fmt.Sprintf("0x%x", reply.ClockId),
 		Type:       GetDpllType(reply.Type),
 	}
+}
+
+// DoPinGetReply is used with the DoPinGet method.
+type DoPinGetReplyHR struct {
+	Id                        uint32            `json:"id"`
+	ClockId                   uint64            `json:"clockId"`
+	BoardLabel                string            `json:"boardLabel"`
+	PanelLabel                string            `json:"panelLabel"`
+	PackageLabel              string            `json:"packageLabel"`
+	Type                      string            `json:"type"`
+	Frequency                 uint64            `json:"frequency"`
+	FrequencySupported        FrequencyRange    `json:"frequencySupported"`
+	Capabilities              string            `json:"capabilities"`
+	ParentDevice              PinParentDeviceHR `json:"pinParentDevice"`
+	ParentPin                 PinParentPinHR    `json:"pinParentPin"`
+	PhaseAdjustMin            int32             `json:"phaseAdjustMin"`
+	PhaseAdjustMax            int32             `json:"phaseAdjustMax"`
+	PhaseAdjust               int32             `json:"phaseAdjust"`
+	FractionalFrequencyOffset int               `json:"fractionalFrequencyOffset"`
+	ModuleName                string            `json:"moduleName"`
+}
+
+// PinParentDevice contains nested netlink attributes.
+type PinParentDeviceHR struct {
+	ParentId    uint32 `json:"parentId"`
+	Direction   string `json:"direction"`
+	Prio        uint32 `json:"prio"`
+	State       string `json:"state"`
+	PhaseOffset int64  `json:"phaseOffset"`
+}
+
+// PinParentPin contains nested netlink attributes.
+type PinParentPinHR struct {
+	ParentId uint32 `json:"parentId"`
+	State    string `json:"parentState"`
+}
+
+// GetPinState returns DPLL pin state as a string
+func GetPinState(s uint32) string {
+	stateMap := map[int]string{
+		1: "connected",
+		2: "disconnected",
+		3: "selectable",
+	}
+	r, found := stateMap[int(s)]
+	if found {
+		return r
+	}
+	return ""
+}
+
+// GetPinType returns DPLL pin type as a string
+func GetPinType(tp uint32) string {
+	typeMap := map[int]string{
+		1: "mux",
+		2: "ext",
+		3: "synce-eth-port",
+		4: "int-oscillator",
+		5: "gnss",
+	}
+	typ, found := typeMap[int(tp)]
+	if found {
+		return typ
+	}
+	return ""
+}
+
+// GetPinDirection returns DPLL pin direction as a string
+func GetPinDirection(d uint32) string {
+	directionMap := map[int]string{
+		1: "input",
+		2: "output",
+	}
+	dir, found := directionMap[int(d)]
+	if found {
+		return dir
+	}
+	return ""
+}
+
+// GetPinCapabilities returns DPLL pin capabilities as a csv
+func GetPinCapabilities(c uint32) string {
+	cMap := map[int]string{
+		0: "",
+		1: "direction-can-change",
+		2: "priority-can-change",
+		3: "direction-can-change,priority-can-change",
+		4: "state-can-change",
+		5: "state-can-change,direction-can-change",
+		6: "state-can-change,priority-can-change",
+		7: "state-can-change,direction-can-change,priority-can-change",
+	}
+	cap, found := cMap[int(c)]
+	if found {
+		return cap
+	}
+	return ""
+}
+
+// GetPinInfoHR returns human-readable pin status
+func GetPinInfoHR(reply *DoPinGetReply) ([]byte, error) {
+	hr := DoPinGetReplyHR{
+		Id:                 reply.Id,
+		ClockId:            reply.ClockId,
+		BoardLabel:         reply.BoardLabel,
+		PanelLabel:         reply.PanelLabel,
+		PackageLabel:       reply.PackageLabel,
+		Type:               GetPinType(reply.Type),
+		Frequency:          reply.Frequency,
+		FrequencySupported: reply.FrequencySupported,
+		Capabilities:       GetPinCapabilities(reply.Capabilities),
+		ParentDevice: PinParentDeviceHR{
+			ParentId:    reply.ParentDevice.ParentId,
+			Direction:   GetPinDirection(reply.ParentDevice.Direction),
+			Prio:        reply.ParentDevice.Prio,
+			State:       GetPinState(reply.ParentDevice.State),
+			PhaseOffset: reply.ParentDevice.PhaseOffset,
+		},
+		ParentPin: PinParentPinHR{
+			ParentId: reply.ParentPin.ParentId,
+			State:    GetPinState(reply.ParentPin.State),
+		},
+		PhaseAdjustMin:            reply.PhaseAdjustMin,
+		PhaseAdjustMax:            reply.PhaseAdjustMax,
+		PhaseAdjust:               reply.PhaseAdjust,
+		FractionalFrequencyOffset: reply.FractionalFrequencyOffset,
+		ModuleName:                reply.ModuleName,
+	}
+	return json.Marshal(hr)
 }
