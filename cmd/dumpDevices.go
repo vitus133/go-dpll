bfe8dc3a2239b2fb7f431fae336f6677e8b3a0ef
Vitaly Grinberg <v.g@redhat.com>
2024-03-04 13:07:35 +0000

    Add help

--- cmd/dumpDevices.go
+++ cmd/dumpDevices.go
@@ -15,13 +15,8 @@ import (
 // dumpDevicesCmd represents the dumpDevices command
 var dumpDevicesCmd = &cobra.Command{
 	Use:   "dumpDevices",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Dump DPLL devices",
+	Long:  `Dumps all DPLL devices in JSON format`,
 	Run: func(cmd *cobra.Command, args []string) {
 		conn, err := dpll.Dial(nil)
 		if err != nil {
